4a3398fc10bbfc28f10c8d2c79c4c975a0d8d05f
Iago <iagofalcom@hotmail.com>
2019-10-21 23:47:47 -0300

    Implementando ClienteDAO

--- src/main/java/br/com/unipac/cliente/api/clienteapi/resources/ClienteResources.java
+++ src/main/java/br/com/unipac/cliente/api/clienteapi/resources/ClienteResources.java
@@ -1,10 +1,14 @@
 package br.com.unipac.cliente.api.clienteapi.resources;
 
+import br.com.unipac.cliente.api.clienteapi.models.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import br.com.unipac.cliente.api.clienteapi.daoInterfaces.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/clientes")
@@ -12,7 +16,6 @@ public class ClienteResources {
 	
 	@Autowired
 	private ClienteDAO dao;
-	
-	
+
 
 }
